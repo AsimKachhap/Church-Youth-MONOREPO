393a1a299f1f31c7a1ee8e35560da471e8e25a59
unknown <asim26k@gmail.com>
2024-09-14 07:35:51 +0530

    Connect to Database

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 
+import connectDB from "./utils/db.js";
+
 const app = express();
 
 dotenv.config();
 app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 4040;
-
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running at http://localhost:${PORT}`);
 });
