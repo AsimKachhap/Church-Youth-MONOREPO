5114a00f71af630e9a2216f311638922260ad4d5
unknown <asim26k@gmail.com>
2024-10-10 17:29:48 +0530

    UPDATE Noow the User is also etting updated with UserDetails

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import { UserDetails } from "../models/userDetails.model.js";
+import User from "../models/user.model.js";
+import mongoose from "mongoose";
 import uploadOnCloudinary from "../utils/uploadOnCloudinary.js";
 
 // ADDING USER DETAILS
@@ -30,62 +32,103 @@ export const addUserDetails = async (req, res) => {
   const state = parishInfo.state;
   const pin = parishInfo.pin;
 
+  const session = await mongoose.startSession(); // Start a session for transaction
+  session.startTransaction();
+
   try {
     if (req.file) {
-      const localFilePath = req.file.path; // Get the local file path from Multer
-
-      // Upload the photo to Cloudinary
+      const localFilePath = req.file.path;
       const uploadResult = await uploadOnCloudinary(localFilePath);
+      const photoUrl = uploadResult.url;
 
-      if (uploadResult && uploadResult.url) {
-        const photoUrl = uploadResult.url; // Get the URL of the uploaded image
+      if (photoUrl) {
+        // 1. Create user details in the database
+        const userDetails = await UserDetails.create(
+          [
+            {
+              firstName,
+              middleName,
+              lastName,
+              phoneNo,
+              age: Number(age),
+              gender,
+              photo: photoUrl,
+              currentAddress,
+              highestQualification: {
+                degree,
+                college,
+                passingYear,
+              },
+              jobDetails: {
+                jobTitle,
+                company,
+                location,
+              },
+              parishInfo: {
+                homeParish,
+                district,
+                state,
+                pin,
+              },
+              churchContribution,
+            },
+          ],
+          { session } // Ensure this operation is part of the transaction
+        );
 
-        const userDetails = await UserDetails.create({
-          firstName,
-          middleName,
-          lastName,
-          phoneNo,
-          age: Number(age),
-          gender,
-          photo: photoUrl,
-          currentAddress,
-          highestQualification: {
-            degree: degree,
-            college: college,
-            passingYear: passingYear,
-          },
-          jobDetails: {
-            jobTitle: jobTitle,
-            company: company,
-            location: location,
-          },
-          parishInfo: {
-            homeParish: homeParish,
-            district: district,
-            state: state,
-            pin: pin,
-          },
-          churchContribution,
-        });
+        //2. Update the User with new UserDetails
+        try {
+          const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+              userDetails: userDetails[0]._id,
+            },
+            {
+              session,
+              new: true,
+            }
+          );
+
+          console.log(updatedUser);
+        } catch (error) {
+          await session.abortTransaction();
+          session.endSession();
+
+          res.status(400).json({
+            message:
+              "Something went wrong while updating User with UserDetails.",
+          });
+        }
+
+        // Commit the transaction after all operations succeed
+        await session.commitTransaction();
+        session.endSession();
 
         res.status(201).json({
           message: "User details saved successfully",
           data: userDetails,
         });
       } else {
+        // Abort the transaction if image upload fails
+        await session.abortTransaction();
+        session.endSession();
+
         res.status(400).json({
-          message: "Failed to upload photo to Cloudinary.",
+          message:
+            "Something went wrong while uploading the photo to Cloudinary.",
         });
-        console.log("Cloudinary did not return a URL for the uploaded image.");
       }
     } else {
-      res.status(404).json({ message: "No photo found in the request." });
+      res.status(404).json({ message: "Photo not provided." });
     }
   } catch (error) {
+    // Abort the transaction on error
+    await session.abortTransaction();
+    session.endSession();
+
     res.status(500).json({
       message: "Something went wrong on the server.",
       error: error.message,
     });
-    console.log(error);
   }
 };
