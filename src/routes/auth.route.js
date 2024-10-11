e4f6b20222af59463bafcf39cd981887db61c7c9
unknown <asim26k@gmail.com>
2024-10-11 10:50:50 +0530

    FEATURE: Refresh Access Token

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,17 @@
 import express from "express";
 
-import { registerUser, login, logout } from "../controllers/auth.controller.js";
+import {
+  registerUser,
+  login,
+  logout,
+  refreshAccessToken,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/refresh-access-token", refreshAccessToken);
 
 export default router;
